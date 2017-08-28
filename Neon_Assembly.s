dcece33d86246b12ee0cb9363b204d9f07077026
RajibTheKing <aporba.das@gmail.com>
2017-08-28 16:42:41 +0600

    crop_420 assembly code reduced with same time complexity

--- Neon_Assembly.s
+++ Neon_Assembly.s
@@ -261,13 +261,17 @@ vmov r7, r8, d2
 mul r9, r8, r4
 add r0, r0, r9
 
-#Now Ready to process U
+
 
 
 vshr.u32 d0, d0, #1
 vshr.u32 d1, d1, #1
 vshr.u32 d2, d2, #1
 
+mov r11, #0
+
+#Now Ready to process UV
+.crop_yuv420_arm_neon_UV_Process:
 vmov r3, r4, d0
 vmov r5, r6, d1
 vmov r7, r8, d2
@@ -275,7 +279,7 @@ vmov r7, r8, d2
 mul r9, r7, r4
 add r0, r0, r9
 sub r8, r3, r8
-.crop_yuv420_arm_neon_U_Height:
+.crop_yuv420_arm_neon_UV_Height:
 
     vmov r5, r6, d1
     add r0, r0, r5
@@ -283,27 +287,27 @@ sub r8, r3, r8
 
     sub r9, r6, r5
     ands r9, r9, #7
-    beq .crop_yuv420_arm_neon_U_Width
+    beq .crop_yuv420_arm_neon_UV_Width
     vld1.u8 {d4}, [r0], r9
     vst1.u8 {d4}, [r1], r9
 
     add r5, r5, r9
     cmp r5, r6
-    beq .crop_yuv420_arm_neon_U_Width_end
+    beq .crop_yuv420_arm_neon_UV_Width_end
 
-    .crop_yuv420_arm_neon_U_Width:
+    .crop_yuv420_arm_neon_UV_Width:
         vld1.u8 {d4}, [r0]!
         vst1.u8 {d4}, [r1]!
         add r5, r5, #8
         cmp r5, r6
-        bne .crop_yuv420_arm_neon_U_Width
+        bne .crop_yuv420_arm_neon_UV_Width
     
-    .crop_yuv420_arm_neon_U_Width_end:
+    .crop_yuv420_arm_neon_UV_Width_end:
 vmov r5, r6, d1
 add r0, r0, r6
 add r7, r7, #1
 cmp r7, r8
-bne .crop_yuv420_arm_neon_U_Height
+bne .crop_yuv420_arm_neon_UV_Height
 
 vmov r3, r4, d0
 vmov r5, r6, d1
@@ -311,50 +315,11 @@ vmov r7, r8, d2
 
 mul r9, r8, r4
 add r0, r0, r9
+add r11, r11, #1
+cmp r11, #2
+bne .crop_yuv420_arm_neon_UV_Process
 
-#Ready To Process V
-vmov r3, r4, d0
-vmov r5, r6, d1
-vmov r7, r8, d2
 
-mul r9, r7, r4
-add r0, r0, r9
-sub r8, r3, r8
-.crop_yuv420_arm_neon_V_Height:
-
-    vmov r5, r6, d1
-    add r0, r0, r5
-    sub r6, r4, r6
-
-    sub r9, r6, r5
-    ands r9, r9, #7
-    beq .crop_yuv420_arm_neon_V_Width
-    vld1.u8 {d4}, [r0], r9
-    vst1.u8 {d4}, [r1], r9
-
-    add r5, r5, r9
-    cmp r5, r6
-    beq .crop_yuv420_arm_neon_V_Width_end
-
-    .crop_yuv420_arm_neon_V_Width:
-        vld1.u8 {d4}, [r0]!
-        vst1.u8 {d4}, [r1]!
-        add r5, r5, #8
-        cmp r5, r6
-        bne .crop_yuv420_arm_neon_V_Width
-    .crop_yuv420_arm_neon_V_Width_end:
-    vmov r5, r6, d1
-    add r0, r0, r6
-    add r7, r7, #1
-    cmp r7, r8
-bne .crop_yuv420_arm_neon_V_Height
-
-vmov r3, r4, d0
-vmov r5, r6, d1
-vmov r7, r8, d2
-
-mul r9, r8, r4
-add r0, r0, r9
 
 
 pop { r3-r11, pc }
