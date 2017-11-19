07a9a4e5d8cad8c8b6834fd3eb31a009c846706e
RajibTheKing <aporba.das@gmail.com>
2017-11-19 17:16:32 +0600

    add new files to test thumbnail

---
+++ ExternalVideoProcessingViewController.h
@@ -0,0 +1,12 @@
+//
+//  ExternalVideoProcessingViewController.h
+//  TestCamera
+//
+//  Created by Rajib Chandra Das on 11/18/17.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExternalVideoProcessingViewController : UIViewController
+
+@end
